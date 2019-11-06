81628d2fc6f4ceecb027d40fe764634fc5d87b40
kcheung3102 <kcheung3102@gmail.com>
2019-11-06 16:52:02 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Scanner userInput = new Scanner(System.in);
+        ArrayList<Transcript> courses = new ArrayList<>();
+
+        String answer = "y";
+        Transcript joe = new Transcript();
+        System.out.println("Welcome to transcript App.");
+        System.out.println("Enter Student ID number: ");
+        joe.setiD(userInput.nextInt());
+        System.out.println("Enter Students First Name");
+        joe.setFirstName(userInput.nextLine());
+        System.out.println("Enter Students Last Name");
+        joe.setLastName(userInput.nextLine());
+
+
     }
 
 
--- src/Validator.java
+++ src/Validator.java
@@ -1,16 +1,10 @@
 public class Validator {
 
-    private static boolean isLettervalid(String str){
-
-        if(str.length() !=1) {
-            return false;
-        }
-        char c = Character.toLowerCase(str.charAt(0));
-        return c >= 'a' && c <= 'f';
+    private static boolean isLettervalid(String userInput){
+        return userInput.matches("[a-zA-F]+");
     }
 
-    private static boolean isCreditValid(int num) {
-        final int maxLength = 1;
-        if(num )
+    private static boolean isInteger(int credit ) {
+        return
     }
 }
